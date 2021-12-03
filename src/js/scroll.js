abdd9b5cda7edf6efd0e9ee20a4d0f99fd53a928
JustAlex322 <88882580+JustAlex322@users.noreply.github.com>
2021-12-03 12:49:48 +0300

    Update 2021-12-03T09:49:06.189Z

--- src/js/scroll.js
+++
@@ -1,7 +0,0 @@
-const getFraseBtn = document.querySelector('.btn');
-
-function scrollToFrase() {
-    console.log('123');
-    document.querySelector('.blockquote-3').scrollIntoView();
-}
-getFraseBtn.addEventListener('click', scrollToFrase);
